64455f2e0dfd3f5a75d84f1e975611a5913770f5
Ahmed EBEN HASSINE 脳の流れ <ahmedbhs123@gmail.com>
2021-05-29 16:18:25 +0200

    Update SolutionJavaCodingProblem.java

--- src/SolutionJavaCodingProblem.java
+++ src/SolutionJavaCodingProblem.java
@@ -443,6 +443,19 @@ public class SolutionJavaCodingProblem {
         }
         return true;
     }
+    
+    function isPrime($number) {
+    //boucle de 2 au nombre à tester
+    for ($i = 2; $i < $number; $i++) {
+        //test du qutotien de la division
+        if ($number % $i == 0) {
+            return FALSE;
+        }
+    }
+ 
+    //Aucun diviseur trouvé, c'est un nombre permier
+    return TRUE;
+}
 
     /*
      * Q19:
